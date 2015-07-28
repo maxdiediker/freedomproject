afe093eb8cc5ba5fa3a9f1dafc4f2fa8c021aa7e
maxdiediker <mdiediker@gmail.com>
2015-07-27 18:20:16 -0700

    changes to category.php
    deleting unecessary info

--- category-about-us.php
+++ category-about-us.php
@@ -5,6 +5,7 @@
 <!-- END SIDEBAR -->
 
 <!-- START CONTENT -->
+<H2>THIS IS THE ABOUT US PAGE!!<H2>
 <div id="content" class="index">
 <?php if (have_posts()) : ?>
 
@@ -12,20 +13,15 @@
     	<div class="post-box">
     	<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
         <p class="postmetadata">
-            <span class="date">Posted on <?php the_time('F jS, Y') ?></span> in
+
             <span class="cat"><?php the_category(', ') ?></span>
-            <span class="author">by <?php the_author_posts_link(); ?></span>
-         	<span class="comments">with <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?></span>
-        </p>
+          
 		<?php the_content('More &raquo;'); ?>
-     	<?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
+
         </div>
     <?php endwhile; ?>
 
-		<nav class="post-nav">
-            <p class="alignleft"><?php next_posts_link('&laquo; Older Posts') ?></p>
-            <p class="alignright"><?php previous_posts_link('Newer Posts &raquo;') ?></p>
-		</nav>
+
 
 <?php endif; ?>
 </div><!-- END CONTENT -->

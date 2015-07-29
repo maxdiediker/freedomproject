a63bae51d67416fd7b69dd116fbd6fdf39040ecf
maxdiediker <mdiediker@gmail.com>
2015-07-29 16:46:34 -0700

    changes to page.php

--- single.php
+++ single.php
@@ -11,21 +11,17 @@
   <div id="content" class="single">
       <strong class="breadcrumb"><?php if (function_exists('bcn_display')) { bcn_display(); } ?></strong>
           <h2><?php the_title(); ?></h2>
-      <p class="postmetadata">
-      	<span class="date">Posted on <?php the_time('F jS, Y') ?></span> in
-  		<span class="cat"><?php the_category(', ') ?></span>
-  		<span class="author">by <?php the_author_posts_link(); ?></span>
+
+
+  	
+
       </p>
       <?php the_content('More &raquo;'); ?>
       <?php wp_link_pages(); ?>
 
-  		<nav class="post-nav">
-  			<div class="alignleft"><?php previous_post_link('&laquo; %link', '%title', true) ?></div>
-  			<div class="alignright"><?php next_post_link('%link &raquo;', '%title', true) ?></div>
-  		</nav>
 
-      <?php edit_post_link('Edit this entry.', '<p><small>', '</small></p>'); ?>
-  	<?php comments_template(); ?>
+
+
       <?php endwhile; else: ?>
       <p>Sorry, no posts matched your criteria.</p>
       <?php endif; ?>
@@ -34,7 +30,7 @@
 
 
 </div>
-<!-- end main -->
+<!-- end main-single -->
 
 
 

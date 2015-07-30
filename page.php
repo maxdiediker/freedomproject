8a9864a49afb57c7abb92474e3b0e57f4af31bbd
maxdiediker <mdiediker@gmail.com>
2015-07-29 17:14:04 -0700

    adds loop back to page.php

--- page.php
+++ page.php
@@ -4,8 +4,19 @@
 <?php get_sidebar( 'primary' ); ?>
 <!-- END SIDEBAR -->
 
-<div id = "main">
-<h2>I am trying to break Github process in div id "main"</h2>
+<!-- START CONTENT -->
+<div id="content" class="page">
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	<div id="post-<?php the_ID(); ?>">
+        <h2><?php the_title(); ?></h2>
+		<?php the_content('<p class="serif">More &raquo;</p>'); ?>
+      
+	</div>
+
+	<?php endwhile; endif; ?>
+
+</div>
+<!-- END CONTENT -->
 
 
 </div>

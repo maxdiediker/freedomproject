092174d319e3b3f2e01366ef30884b109dd76a45
maxdiediker <mdiediker@gmail.com>
2015-08-05 18:50:19 -0700

    adds about us template
    and get child function

--- functions.php
+++ functions.php
@@ -108,3 +108,31 @@ function the_breadcrumb() {
         echo '</ul>';
 }
 /* end breadcrumb function*/
+
+// Get Child Pages
+function get_child_pages() {
+
+	global $post;
+
+	rewind_posts(); // stop any previous loops
+	query_posts(array('post_type' => 'page', 'posts_per_page' => -1, 'post_status' => publish,'post_parent' => $post->ID,'order' => 'ASC','orderby' => 'menu_order')); // query and order child pages
+
+	while (have_posts()) : the_post();
+
+		$childPermalink = get_permalink( $post->ID ); // post permalink
+		$childID = $post->ID; // post id
+		$childTitle = $post->post_title; // post title
+		$childExcerpt = $post->post_excerpt; // post excerpt
+
+		echo '<article id="page-excerpt-'.$childID.'" class="page-excerpt">';
+		echo '<h3><a href="'.$childPermalink.'">'.$childTitle.' &raquo;</a></h3>';
+		echo '<p>'.$childExcerpt.' <a href="'.$childPermalink.'">Read More&nbsp;&raquo;</a></p>';
+		echo '</article>';
+
+	endwhile;
+
+	// reset query
+	wp_reset_query();
+
+}
+//

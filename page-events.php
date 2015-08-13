fdb5e29fc7a964110d4ea25e20bedcd60f45917e
max <mdiediker@gmail.com>
2015-08-12 19:20:46 -0700

    add custom page

---
+++ page-events.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<!-- START SIDEBAR -->
+<?php get_sidebar('primary'); ?>
+<!-- END SIDEBAR -->
+
+
+        <div id="content" class="excerpt-peach">
+
+            <?php get_child_pages(); ?>
+
+</div><!-- END CONTENT -->
+</div> <!--END MAIN-->
+
+<?php get_footer(); ?>

f5a008c777681569d4feb95018d413aeaa64ebaf
markdavid <markdpfaff@gmail.com>
2015-08-17 18:13:57 -0700

    Fixes single.php blank page

--- single.php
+++ single.php
@@ -1,41 +1,31 @@
 <?php get_header(); ?>
 
-
-
 <!-- START SIDEBAR -->
 <?php get_sidebar( 'primary' ); ?>
 <!-- END SIDEBAR -->
 
-<?php add_flexslider(); ?>
+<?php //add_flexslider(); ?>
 
 <div id = "main-single">
   <h3>this is single.php</h3>
   <!-- START CONTENT -->
-  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <?php if (have_posts()) : while(have_posts()) : the_post();?> 
   <div id="content" class="single">
-      <strong class="breadcrumb"><?php if (function_exists('bcn_display')) { bcn_display(); } ?></strong>
           <h2><?php the_title(); ?></h2>
 
-
-  	
-
-      </p>
       <?php the_content('More &raquo;'); ?>
-      <?php wp_link_pages(); ?>
+      <?php //wp_link_pages(); ?>
 
 
 
 
-      <?php endwhile; else: ?>
-      <p>Sorry, no posts matched your criteria.</p>
-      <?php endif; ?>
+    <?php endwhile; endif;?>
   </div>
   <!-- END CONTENT -->
 
 
 </div>
 <!-- end main-single -->
-
 
 
 <?php get_footer(); ?>
